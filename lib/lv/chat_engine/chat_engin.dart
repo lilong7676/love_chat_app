146b0b38f14d24b5ec70c8849c0294e5731d7280
Longlong Li <lilong7676@outlook.com>
2019-07-25 23:33:27 +0800

    'test'

--- lib/lv/chat_engine/chat_engin.dart
+++ lib/lv/chat_engine/chat_engin.dart
@@ -1,11 +1,16 @@
+import 'package:love_chat/providers/userManager.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatEngin {
   ChatEngin() {
-    IO.Socket socket = IO.io('http://localhost:3000', <String, dynamic>{'transports': ['websocket']});
+    UserManager userManager = UserManager();
+    String accessToken = userManager.accessToken;
+    
+
+    IO.Socket socket = IO.io('http://10.0.2.2:3000', <String, dynamic>{'transports': ['websocket']});
     socket.on('connect', (_) {
      print('connect');
-     socket.emit('msg', 'flutter test');
+     socket.emit('userLogin', 'flutter test');
     });
     socket.on('msg', (data) => print(data));
     socket.on('disconnect', (_) => print('disconnect'));
